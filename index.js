624e49c72c31dd80074d0a2758edf7c8ce0bddd5
Nguyen A Tan <nguyenatan0322@gmail.com>
2019-01-23 17:24:01 +0700

    Update button

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vuejs!'
+        message: 'Hello Vuejs!',
+        seen: true,
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
-})
+});
+
+app.message = 'I have changed the data!';
